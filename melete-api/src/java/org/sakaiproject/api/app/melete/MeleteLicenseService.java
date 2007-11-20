95384419206ff62b6c51f3c69063202b42ae951f
MaheshwariRashmi@foothill.edu <MaheshwariRashmi@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2007-11-20 21:09:22 +0000

    moved to trunk
    git-svn-id: https://source.sakaiproject.org/contrib/foothill/melete/trunk@43646 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ melete-api/src/java/org/sakaiproject/api/app/melete/MeleteLicenseService.java
@@ -0,0 +1,41 @@
+/**********************************************************************************
+*
+* $Header: /usr/src/sakai/melete-2.4/melete-api/src/java/org/sakaiproject/api/app/melete/MeleteLicenseService.java,v 1.2 2007/05/07 21:16:21 mallikat Exp $
+*
+***********************************************************************************
+*
+* Copyright (c) 2004, 2005, 2006, 2007 Foothill College, ETUDES Project  
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you 
+* may not use this file except in compliance with the License. You may 
+* obtain a copy of the License at 
+*   
+* http://www.apache.org/licenses/LICENSE-2.0 
+*   
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+* implied. See the License for the specific language governing 
+* permissions and limitations under the License. 
+*
+**********************************************************************************/
+package org.sakaiproject.api.app.melete;
+
+/**
+ * Filename:
+ * Description:
+ * Author:
+ * Date:
+ * Copyright 2004, Foothill College
+ */
+public interface MeleteLicenseService {
+	public abstract Integer getCode();
+
+	public abstract void setCode(Integer code);
+
+	public abstract String getDescription();
+
+	public abstract void setDescription(String description);
+
+	public abstract String toString();
+}
