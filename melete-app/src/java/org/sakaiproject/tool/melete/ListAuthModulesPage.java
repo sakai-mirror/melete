3841276fd7e3b412e91b319c68e0aef904f9f764
thoppaymallika@fhda.edu <thoppaymallika@fhda.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-02-11 21:19:19 +0000

    date fix changes to add and edit module
    git-svn-id: https://source.sakaiproject.org/contrib/foothill/melete/branches/melete_2-4-5_newdatefix@45452 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/sakaiproject/tool/melete/ListAuthModulesPage.java
+++ melete-app/src/java/org/sakaiproject/tool/melete/ListAuthModulesPage.java
@@ -862,50 +862,79 @@ public class ListAuthModulesPage implements Serializable
 		FacesContext ctx = null;
 		ResourceLoader bundle = null;
 		boolean dateErrFlag = false;
+		boolean yearTooBigFlag = false;
 		errModuleIds = new ArrayList();
 		try
 		{
 			Iterator moduleIter = moduleDateBeans.iterator();
 			ctx = FacesContext.getCurrentInstance();
 			bundle = new ResourceLoader("org.sakaiproject.tool.melete.bundle.Messages");
+			Calendar stCal = null;
+			Calendar enCal = null;
 			while (moduleIter.hasNext())
 			{
 				ModuleDateBean mdbean = (ModuleDateBean) moduleIter.next();
+				mdbean.setDateFlag(false);
+				if (mdbean.getModuleShdate().getStartDate() != null)
+				{
+					stCal = Calendar.getInstance();	
+					stCal.setTime(mdbean.getModuleShdate().getStartDate());
+					if (stCal.get(Calendar.YEAR) > 9999)
+					{
+					  yearTooBigFlag = true;
+					  mdbean.setDateFlag(true);
+					}
+				}
+				if (mdbean.getModuleShdate().getEndDate() != null)
+				{
+					enCal = Calendar.getInstance();	
+					enCal.setTime(mdbean.getModuleShdate().getEndDate());
+					if (enCal.get(Calendar.YEAR) > 9999)
+					{
+					  yearTooBigFlag = true;
+					  mdbean.setDateFlag(true);
+					}
+				}
 				if ((mdbean.getModuleShdate().getStartDate() != null)&&(mdbean.getModuleShdate().getEndDate() != null))
-				{	
+				{
 				  if (mdbean.getModuleShdate().getStartDate().compareTo(mdbean.getModuleShdate().getEndDate()) >= 0)
 				  {
 					dateErrFlag = true;
 					mdbean.setDateFlag(true);
-					errModuleIds.add(mdbean);
 					/*
 					 * addDateErrorMessage(ctx); return "list_auth_modules";
 					 */
 				  }
-				  else
+				  
+			     }
+				if (mdbean.isDateFlag() == true)
 				  {
-					mdbean.setDateFlag(false);
+					  errModuleIds.add(mdbean);
 				  }
-				}
-				else
-				{
-					mdbean.setDateFlag(false);
-				}
 			}
-			getModuleService().updateProperties(moduleDateBeans);
-
-			if (dateErrFlag == true)
-			{
+			  getModuleService().updateProperties(moduleDateBeans);
+		  
+			
+			if ((yearTooBigFlag == true)||(dateErrFlag == true))
+			{	
+			  if (yearTooBigFlag == true)
+			  {
+			  String msg = bundle.getString("year_toobig_error");
+			  addMessage(ctx, "Year Error", msg, FacesMessage.SEVERITY_ERROR);
+			  }
+			  if (dateErrFlag == true)
+			  {
 				String msg = bundle.getString("date_error");
 				addMessage(ctx, "Date Error", msg, FacesMessage.SEVERITY_ERROR);
-			}
+			  }
+			}  
 			else
 			{
 				String msg = bundle.getString("changes_saved");
 				addMessage(ctx, "Changes Saved", msg, FacesMessage.SEVERITY_INFO);
 			}
-
-		}
+			}
+	
 		catch (Exception e)
 		{
 			// e.printStackTrace();
