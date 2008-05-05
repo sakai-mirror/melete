f18a78385c087a62c784fa27155e4d07e4c9b50d
thoppaymallika@fhda.edu <thoppaymallika@fhda.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-05-05 22:54:39 +0000

    added site preference table
    git-svn-id: https://source.sakaiproject.org/contrib/foothill/melete/trunk@48645 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/mysql/melete25_upgrade.sql
+++ components/src/sql/mysql/melete25_upgrade.sql
@@ -12,3 +12,8 @@ CREATE TABLE `melete_bookmarks` (
 alter table melete_section add OPEN_WINDOW tinyint(1);
 update melete_section set OPEN_WINDOW=1;
 drop table melete_module_student_privs;
+CREATE TABLE `melete_site_preference` (
+  `PREF_SITE_ID` varchar(99) NOT NULL default '',
+  `PRINTABLE` tinyint(1) default NULL,
+  PRIMARY KEY (`PREF_SITE_ID`)
+  );
