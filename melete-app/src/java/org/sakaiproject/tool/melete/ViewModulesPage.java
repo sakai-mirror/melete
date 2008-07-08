92e89b077edd01c69224a58e0a950f34cbf415b2
MaheshwariRashmi@foothill.edu <MaheshwariRashmi@foothill.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-07-08 20:46:57 +0000

    auto number cache issue
    git-svn-id: https://source.sakaiproject.org/contrib/foothill/melete/trunk@50845 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/sakaiproject/tool/melete/ViewModulesPage.java
+++ melete-app/src/java/org/sakaiproject/tool/melete/ViewModulesPage.java
@@ -295,7 +295,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
 		vsPage.setSection(secBean.getSection());
 		// added by rashmi on 6/14/05
 		vsPage.setModule(null);
-		vsPage.setAutonumber(this.autonumber);
+		//vsPage.setAutonumber(this.autonumber);
 		String retVal = "view_section_student";
 		if (isAuthor) retVal = "view_section";
 
@@ -314,6 +314,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
     	ListModulesPage listPage = (ListModulesPage)
             binding.getValue(context);
     	listPage.setModuleDateBeans(null);
+    	listPage.setAutonumberMaterial(null);
 		if (getInstRole()) return "list_modules_inst";
 		else return "list_modules_student";
 	}
@@ -336,6 +337,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
  	            vsPage.setSection(secBean.getSection());
  	            //added by rashmi on 6/14/05
  	            vsPage.setModule(null);
+ 	            vsPage.setAutonumber(null);
 
 
  	     String retVal = "view_section_student";
@@ -374,7 +376,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
  	            vsPage.setSection(secBean.getSection());
  	            //added by rashmi on 6/14/05
  	            vsPage.setModule(null);
-
+ 	            vsPage.setAutonumber(null);
  	     String retVal = "view_section_student";
 
  	    //03/10/05  rashmi - added seperate page for links and upload to show them in frame
