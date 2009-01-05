8bc8b8ceef6da8ad218dec74afe3d7bbc5277c2d
rashmi@etudes.org <rashmi@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-01-05 19:25:50 +0000

    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@56545 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-api/src/java/org/etudes/api/app/melete/util/XMLHelper.java
+++ melete-api/src/java/org/etudes/api/app/melete/util/XMLHelper.java
@@ -1,7 +1,7 @@
 /**********************************************************************************
  *
- * $URL$ 
- * $Id$  
+ * $URL$
+ * $Id$
  ***********************************************************************************
  *
  * Copyright (c) 2008 Etudes, Inc.
@@ -11,7 +11,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -38,12 +38,8 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-/**
- * @author Foothill College
- */
-
 public class XMLHelper {
-	
+
 	protected static final String DEFAULT_PARSER_NAME = "org.apache.xerces.parsers.SAXParser";
 	/** Validation feature id (http://xml.org/sax/features/validation). */
     protected static final String VALIDATION_FEATURE_ID = "http://xml.org/sax/features/validation";
@@ -53,8 +49,8 @@ public class XMLHelper {
     protected static final String DYNAMIC_VALIDATION_FEATURE_ID = "http://apache.org/xml/features/validation/dynamic";
     /** Namespace prefixes feature id (http://xml.org/sax/features/namespace-prefixes). */
     protected static final String NAMESPACE_PREFIXES_FEATURE_ID = "http://xml.org/sax/features/namespace-prefixes";
-    
-	
+
+
 	/**
 	 * Creates xml docuemnt and adds element
 	 * @param element
@@ -62,12 +58,12 @@ public class XMLHelper {
     static public Document createXMLDocument(Element element) {
 		Document document = DocumentHelper.createDocument();
 		document.add(element);
-		
+
 		return document;
 	}
-	
+
 	/**
-	 * Reads a Document from the given File 
+	 * Reads a Document from the given File
 	 * @param xmlFile - is the File to read from
 	 * @throws Exception
 	 */
@@ -82,12 +78,12 @@ public class XMLHelper {
 		}catch (Exception e) {
 			throw e;
 		}
-		
+
     }
-	
+
 	/**
 	 * creates the SAXReader
-	 * @return - the SAXReader instance 
+	 * @return - the SAXReader instance
 	 * @throws SAXException
 	 */
 	static public SAXReader getSaxReader()throws SAXException{
@@ -97,11 +93,11 @@ public class XMLHelper {
 		} catch (SAXException se) {
 			throw se;
 		}
-		
+
 		return new SAXReader(xmlreader);
 	}
-	
-	
+
+
 	/**
 	 * creates the xml reader and sets the features
 	 * @return - returns the XMLreader
@@ -114,10 +110,10 @@ public class XMLHelper {
 		xmlreader.setFeature(SCHEMA_VALIDATION_FEATURE_ID, true);
 		xmlreader.setFeature(VALIDATION_FEATURE_ID, true);
 		xmlreader.setFeature(DYNAMIC_VALIDATION_FEATURE_ID, true);
-		
+
 		return xmlreader;
 	}
-	
+
 	/**
 	 * creates xml file for the give path
 	 * @param url - path to create xml file
@@ -129,7 +125,7 @@ public class XMLHelper {
 		try {
 			if (document == null)
 				document = DocumentHelper.createDocument();
-			
+
 			out = new FileOutputStream(new File(url));
 			OutputFormat outformat = OutputFormat.createPrettyPrint();
 			//outformat.setEncoding(aEncodingScheme);
@@ -146,7 +142,7 @@ public class XMLHelper {
 					writer.close();
 			} catch (IOException e2) {
 			}
-			
+
 			try {
 				if (out != null)
 					out.close();
