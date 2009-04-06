e986afe0806f1f06cb62da1e4417baeddbcd9c30
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-04-06 23:02:57 +0000

    Fix for ME-938
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@59696 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-util/src/java/org/imsglobal/simplelti/SimpleLTIUtil.java
+++ melete-util/src/java/org/imsglobal/simplelti/SimpleLTIUtil.java
@@ -3,7 +3,7 @@
  * $Id$
  **********************************************************************************
  *
- * Copyright (c) 2008 Etudes, Inc.
+ * Copyright (c) 2008, 2009 Etudes, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
