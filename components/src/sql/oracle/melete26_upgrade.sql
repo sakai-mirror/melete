152f9bbf5e6ec52fddb3822000fc6029f411342d
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-04-06 22:54:47 +0000

    Fix for ME-938
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@59694 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/oracle/melete26_upgrade.sql
+++ components/src/sql/oracle/melete26_upgrade.sql
@@ -2,7 +2,7 @@
 -- $URL$
 -- $Id$
 -- *********************************************************************
---  Copyright (c) 2008 Etudes, Inc.  
+--  Copyright (c) 2008, 2009 Etudes, Inc.  
 
 --   Licensed under the Apache License, Version 2.0 (the "License"); you  
 --   may not use this file except in compliance with the License. You may  
