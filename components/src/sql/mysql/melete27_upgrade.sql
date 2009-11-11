f17b0bab4c848e70d2516f2d240600c5db8377d3
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-11-11 23:04:19 +0000

    newer field sizes for copyright_owner and cc_license_url in melete_user_preference table
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@64622 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/mysql/melete27_upgrade.sql
+++ components/src/sql/mysql/melete27_upgrade.sql
@@ -1,9 +1,9 @@
 alter table melete_user_preference add column LICENSE_CODE int(11);
-alter table melete_user_preference add column CC_LICENSE_URL varchar(70);
+alter table melete_user_preference add column CC_LICENSE_URL varchar(275);
 alter table melete_user_preference add column REQ_ATTR tinyint(1);
 alter table melete_user_preference add column ALLOW_CMRCL tinyint(1);
 alter table melete_user_preference add column ALLOW_MOD int(11);
-alter table melete_user_preference add column COPYRIGHT_OWNER varchar(55);
+alter table melete_user_preference add column COPYRIGHT_OWNER varchar(255);
 alter table melete_user_preference add column COPYRIGHT_YEAR varchar(25);
 
 alter table melete_module_shdates add column START_EVENT_ID varchar(99);
