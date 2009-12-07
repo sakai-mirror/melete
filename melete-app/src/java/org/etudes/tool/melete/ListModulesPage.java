3508eca10795518ee6e23f20921c0820d5b1f038
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2009-12-07 19:21:04 +0000

    Fix for ME-1113(arrayindexoutofboundsexception)
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@65085 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/etudes/tool/melete/ListModulesPage.java
+++ melete-app/src/java/org/etudes/tool/melete/ListModulesPage.java
@@ -523,6 +523,8 @@ public class ListModulesPage implements Serializable{
 			if ((viewModuleBeans != null)&&(viewModuleBeans.size() > 0))
 			{
 		    	vmBean = (ViewModBean) viewModuleBeans.get(selModIndex);
+		    	//Fix for ArrayIndexOutofBoundsException: -1
+		    	if (selSecIndex == -1) selSecIndex = 0;
 			    vsBean = (ViewSecBean) vmBean.getVsBeans().get(selSecIndex);
 		        modSeqNo = vmBean.getSeqNo();
 			}
