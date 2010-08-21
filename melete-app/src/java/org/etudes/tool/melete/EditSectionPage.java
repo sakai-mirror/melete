e0aed350abd4c86e55a898fe63a6de55f91a792b
rashmi@etudes.org <rashmi@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-08-21 19:05:19 +0000

    Edit contents from FCKEditor are not saving
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/tags/2.8m2@69854 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/etudes/tool/melete/EditSectionPage.java
+++ melete-app/src/java/org/etudes/tool/melete/EditSectionPage.java
@@ -385,6 +385,8 @@ public class EditSectionPage extends SectionPage implements Serializable
 	    				if (meleteResource != null && meleteResource.getResourceId() != null && meleteResource.getResourceId().length() == 0)
 	    				{
 	      					throw new MeleteException("section_html_null");
+	      					getMeleteCHService().checkResource(meleteResource.getResourceId());
+	    					editMeleteCollectionResource(meleteResource.getResourceId());
 	    				}
 	    			}
 	    			//The condition below was put in to handle ME-639
