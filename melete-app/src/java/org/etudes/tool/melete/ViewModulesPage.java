ab72c9bd47768cb5dd8957fc6187df9090b3184e
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-08-25 18:47:59 +0000

    Fix for ME-1265, what's next
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@69917 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/etudes/tool/melete/ViewModulesPage.java
+++ melete-app/src/java/org/etudes/tool/melete/ViewModulesPage.java
@@ -185,6 +185,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
     	  }
     	  if (this.mdbean != null)
     	  {
+    		this.moduleId = this.mdbean.getModuleId();  
     	    this.moduleSeqNo = this.mdbean.getModule().getCoursemodule().getSeqNo();
     	    this.prevSeqNo = getModuleService().getPrevSeqNo(userId, courseId,this.moduleSeqNo);
   	  	    this.nextSeqNo = getModuleService().getNextSeqNo(userId, courseId,this.moduleSeqNo);
@@ -371,6 +372,7 @@ public class ViewModulesPage implements Serializable/*,ToolBean*/ {
         	vnPage.setPrevSecId(0);
           }
           vnPage.setPrevModId(this.prevMdbean.getModule().getModuleId());
+          vnPage.setModule(null);
           //vnPage.setModule(this.prevMdbean.getModule());
         }
         else
