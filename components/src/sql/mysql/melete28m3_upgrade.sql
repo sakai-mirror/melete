49a85f05d29f037ccbd603b52110b2e933fa7eac
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2010-09-29 16:51:30 +0000

    Fix for ME-1287, moving of special access table creation to m4 as it is getting release in m4, changing copyright_year column size in m3.sql and m4.sql
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@70466 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/mysql/melete28m3_upgrade.sql
+++ components/src/sql/mysql/melete28m3_upgrade.sql
@@ -1,11 +1,2 @@
-CREATE TABLE `melete_special_access` (
-  `ACCESS_ID` int(11) NOT NULL default '0',
-  `MODULE_ID` int(11) NOT NULL default '0',
-  `USERS` longtext NOT NULL,
-  `START_DATE` datetime default NULL,
-  `END_DATE` datetime default NULL,
-  PRIMARY KEY  (`ACCESS_ID`),
-  CONSTRAINT `FK_MSA_MM` FOREIGN KEY(`MODULE_ID`) REFERENCES `melete_module`(`MODULE_ID`)
-); 
 alter table melete_resource modify column COPYRIGHT_YEAR varchar(255);
 alter table melete_user_preference modify column COPYRIGHT_YEAR varchar(255);
--- components/src/sql/mysql/melete28m4.sql
+++ components/src/sql/mysql/melete28m4.sql
@@ -162,3 +162,12 @@ CREATE TABLE `melete_bookmark` (
   CONSTRAINT `FK_MB_MS` FOREIGN KEY(`SECTION_ID`) REFERENCES `melete_section`(`SECTION_ID`)
 );
 
+CREATE TABLE `melete_special_access` (
+  `ACCESS_ID` int(11) NOT NULL default '0',
+  `MODULE_ID` int(11) NOT NULL default '0',
+  `USERS` longtext NOT NULL,
+  `START_DATE` datetime default NULL,
+  `END_DATE` datetime default NULL,
+  PRIMARY KEY  (`ACCESS_ID`),
+  CONSTRAINT `FK_MSA_MM` FOREIGN KEY(`MODULE_ID`) REFERENCES `melete_module`(`MODULE_ID`)
+); 
