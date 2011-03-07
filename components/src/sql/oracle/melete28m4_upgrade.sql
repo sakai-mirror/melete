df021628a639cc12100fa26362b79f7e1f3e44f5
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-03-07 20:18:14 +0000

    Replacing LONGTEXT with CLOB for Oracle scripts, ME-1313/ ME-1320
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/tags/2.8.2@73052 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/oracle/melete28m4_upgrade.sql
+++ components/src/sql/oracle/melete28m4_upgrade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE melete_special_access (
   ACCESS_ID number(11) default '0',
   MODULE_ID number(11) default '0',
-  USERS varchar2(4000) NOT NULL,
+  USERS CLOB NOT NULL,
   START_DATE timestamp default NULL,
   END_DATE timestamp default NULL,
   PRIMARY KEY (ACCESS_ID),
--- components/src/sql/oracle/melete_272-282_upgrade.sql
+++ components/src/sql/oracle/melete_272-282_upgrade.sql
@@ -16,7 +16,7 @@ alter table melete_user_preference modify COPYRIGHT_YEAR varchar2(255);
 CREATE TABLE melete_special_access (
   ACCESS_ID number(11) default '0',
   MODULE_ID number(11) default '0',
-  USERS varchar2(4000) NOT NULL,
+  USERS CLOB NOT NULL,
   START_DATE timestamp default NULL,
   END_DATE timestamp default NULL,
   PRIMARY KEY (ACCESS_ID),
