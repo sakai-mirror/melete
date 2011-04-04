8190cebf5e7a48dd8a3dae164acd62e59fe9950d
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-04-04 22:24:33 +0000

    Documenting APIs
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@73561 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/etudes/tool/melete/Util.java
+++ melete-app/src/java/org/etudes/tool/melete/Util.java
@@ -4,7 +4,7 @@
  *
  * **********************************************************************************
  *
- * Copyright (c) 2008 Etudes, Inc.
+ * Copyright (c) 2008, 2011 Etudes, Inc.
  *
  * Portions completed before September 1, 2008 Copyright (c) 2004, 2005, 2006, 2007, 2008 Foothill College, ETUDES Project
  *
@@ -37,27 +37,58 @@ import javax.faces.el.ValueBinding;
 import org.etudes.api.app.melete.exception.MeleteException;
 import org.etudes.api.app.melete.exception.UserErrorException;
 
-public class Util {
+public class Util
+{
 
+	/**
+	 * Pattern is restricted to form tags so that their action can take place. For html tag etc, the sferyx content for fonts test doc which had double quotes and single quote renders bad characters.
+	 * 
+	 * @param str
+	 *        String to check pattern on
+	 * @return true if string contains pattern, false if not
+	 */
+	public static boolean FindNestedHTMLTags(String str)
+	{
+		if (str == null) return false;
+		// look for html or form tags in the content
+		// Pattern pForm = Pattern.compile("<\\s*[hH][tT][mM][lL]\\s*| <\\s*[hH][eE][aA][dD]\\s*| <\\s*[bB][oO][dD][yY]\\s*| <\\s*[fF][oO][rR][mM]\\s*");
+		Pattern pForm = Pattern.compile("<\\s*[fF][oO][rR][mM]\\s*");
+		Matcher m = pForm.matcher(str);
+		if (m.find()) return true;
 
-	public static ValueBinding getBinding(String bindingName) {
-		ApplicationFactory factory = (ApplicationFactory) FactoryFinder
-				.getFactory(FactoryFinder.APPLICATION_FACTORY);
-		ValueBinding binding = factory.getApplication().createValueBinding(
-				bindingName);
+		return false;
+	}
+
+	/** Get ValueBinding for this binding
+	 * @param bindingName String value of binding name
+	 * @return ValueBinding
+	 */
+	public static ValueBinding getBinding(String bindingName)
+	{
+		ApplicationFactory factory = (ApplicationFactory) FactoryFinder.getFactory(FactoryFinder.APPLICATION_FACTORY);
+		ValueBinding binding = factory.getApplication().createValueBinding(bindingName);
 		return binding;
 	}
 
-	//Diego del Blanco y David Roldán - 2/14/06 -- For avoiding the xxxxxxx
-	// patterns...
-	public static String replace(String s, String one, String another) {
-		// In a string replace one substring with another
-		if (s.equals(""))
-			return "";
+	/**
+	 * In a string replace one substring with another
+	 * 
+	 * @param s
+	 *        String to replace in
+	 * @param one
+	 *        String to replace
+	 * @param another
+	 *        String to replace with
+	 * @return Replaced/changed string
+	 */
+	public static String replace(String s, String one, String another)
+	{
+		if (s.equals("")) return "";
 		String res = "";
 		int i = s.indexOf(one, 0);
 		int lastpos = 0;
-		while (i != -1) {
+		while (i != -1)
+		{
 			res += s.substring(lastpos, i) + another;
 			lastpos = i + one.length();
 			i = s.indexOf(one, lastpos);
@@ -66,43 +97,34 @@ public class Util {
 		return res;
 	}
 
-	 /*
-     * validate uploaded file name to see there are no reserver characters
-     * only allowed pattern is [a-zA-z0-9]_-.
-     * revised on 3/31 to remove path name
-     */
-    public static void validateUploadFileName(String name) throws MeleteException
-    {
-            if(name.indexOf("#") != -1)
-	  	  	{
-	  	    throw new MeleteException("embed_img_bad_filename");
-	  	  	}
-    }
+	/**
+	 * This method throws a MeleteException if the url does not start with http:// or https://
+	 * 
+	 * @param linkUrl
+	 *        value of link to be tested
+	 * @throws UserErrorException
+	 *         Exception throw if url does not start with http:// or https://
+	 */
+	public static void validateLink(String linkUrl) throws UserErrorException
+	{
+		if (!(linkUrl.startsWith("http://") || linkUrl.startsWith("https://")))
+		{
+			throw new UserErrorException("add_section_bad_url_format");
+		}
+	}
 
-    /*
-     * This method throws a MeleteException if the url does not start with
-     * http:// or https://
-     */
-    public static void validateLink(String linkUrl) throws UserErrorException
-    {
-    	 if( !(linkUrl.startsWith("http://") || linkUrl.startsWith("https://")))
-    	{
-    	  throw new UserErrorException("add_section_bad_url_format");
-    	}
-    }
-
-    // Pattern is restricted to form tags so that their action can take place.
-    // For html tag etc, the sferyx content for fonts test doc which had double quotes and
-    // single quote renders bad characters. 
-    public static boolean FindNestedHTMLTags(String str)
-    {
-    	if(str == null) return false;
-    	// look for html or form tags in the content
-  //  	Pattern pForm = Pattern.compile("<\\s*[hH][tT][mM][lL]\\s*| <\\s*[hH][eE][aA][dD]\\s*| <\\s*[bB][oO][dD][yY]\\s*| <\\s*[fF][oO][rR][mM]\\s*");
-    	Pattern pForm = Pattern.compile("<\\s*[fF][oO][rR][mM]\\s*");
-    	Matcher m = pForm.matcher(str);
-    	if (m.find()) return true;	
-    
-    	return false;
-    }
+	/**
+	 * validate uploaded file name to see there are no reserver characters only allowed pattern is [a-zA-z0-9]_-. revised on 3/31 to remove path name
+	 * 
+	 * @param name
+	 *        file name
+	 * @throws MeleteException
+	 */
+	public static void validateUploadFileName(String name) throws MeleteException
+	{
+		if (name.indexOf("#") != -1)
+		{
+			throw new MeleteException("embed_img_bad_filename");
+		}
+	}
 }
