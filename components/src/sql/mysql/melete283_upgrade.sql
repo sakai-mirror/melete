f62bc7085d40feb571a1ac6f01ebade5065583a5
rashmi@etudes.org <rashmi@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-05-13 20:30:22 +0000

    remove unwanted scripts
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/tags/2.9m1@74255 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/mysql/melete283_upgrade.sql
+++
@@ -1,4 +0,0 @@
-alter table melete_special_access add column OVERRIDE_START tinyint(1) default '0';
-alter table melete_special_access add column OVERRIDE_END tinyint(1) default '0';
-update melete_special_access set OVERRIDE_START = 1;
-update melete_special_access set OVERRIDE_END = 1;
