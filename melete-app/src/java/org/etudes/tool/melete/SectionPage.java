8b1f318ed742b2ee677d43620a706132a5d99249
rashmi@etudes.org <rashmi@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2012-11-26 21:52:42 +0000

    integrate ckeditor
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/trunk@82144 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- melete-app/src/java/org/etudes/tool/melete/SectionPage.java
+++ melete-app/src/java/org/etudes/tool/melete/SectionPage.java
@@ -1167,8 +1167,9 @@ public abstract class SectionPage implements Serializable
 	public void setFCKCollectionAttrib()
 	{
 		FCK_CollId = getMeleteCHService().getUploadCollectionId(getCurrentCourseId());
-		String attrb = "fck.security.advisor." + FCK_CollId;
-
+		String attrb = "ck.security.advisor." + FCK_CollId;
+		
+		SessionManager.getCurrentSession().setAttribute("ck.collectionId",FCK_CollId);
 		SessionManager.getCurrentSession().setAttribute(attrb, new SecurityAdvisor()
 		{
 			public SecurityAdvice isAllowed(String userId, String function, String reference)
