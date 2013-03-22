8104e1991a8a79c3fc2353a62134f0d9892047f5
mallika@etudes.org <mallika@etudes.org@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2013-03-22 22:09:29 +0000

    Committing oracle upgrade script contributed by user.
    git-svn-id: https://source.sakaiproject.org/contrib/etudes/melete/tags/2.9.1@83160 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- components/src/sql/oracle/melete_282-291_upgrade.sql
+++ components/src/sql/oracle/melete_282-291_upgrade.sql
@@ -1,25 +1,25 @@
-CREATE TABLE `melete_section_track_view` (
-  `SECTION_ID` int(11) NOT NULL default '0',
-  `USER_ID` varchar(99) default NULL,
-  `VIEW_DATE` datetime default NULL,
-  PRIMARY KEY  (`SECTION_ID`,`USER_ID`),
-  CONSTRAINT `FK_MSTV_MS` FOREIGN KEY(`SECTION_ID`) REFERENCES `melete_section`(`SECTION_ID`)
+CREATE TABLE melete_section_track_view (
+  SECTION_ID integer default 0 NOT NULL,
+  USER_ID varchar(99) default NULL,
+  VIEW_DATE date default NULL,
+  PRIMARY KEY  (SECTION_ID,USER_ID),
+  CONSTRAINT FK_MSTV_MS FOREIGN KEY(SECTION_ID) REFERENCES melete_section(SECTION_ID)
 );
 
-alter table melete_special_access add column OVERRIDE_START tinyint(1) default '0';
-alter table melete_special_access add column OVERRIDE_END tinyint(1) default '0';
+alter table melete_special_access add OVERRIDE_START number(1) default '0';
+alter table melete_special_access add OVERRIDE_END number(1) default '0';
 update melete_special_access set OVERRIDE_START = 1;
 update melete_special_access set OVERRIDE_END = 1;
 
-CREATE INDEX USER_ID_IDX ON `melete_section_track_view` (USER_ID);
+CREATE INDEX MSTV_USER_ID_IDX ON melete_section_track_view (USER_ID);
 
-ALTER TABLE melete_cc_license modify column allow_mod int(1) not null;
+ALTER TABLE melete_special_access add ALLOWUNTIL_DATE date default NULL;
 
-ALTER TABLE melete_special_access add column ALLOWUNTIL_DATE datetime default NULL;
-ALTER TABLE melete_special_access add column OVERRIDE_ALLOWUNTIL tinyint(1) default '0';
+ALTER TABLE melete_special_access add OVERRIDE_ALLOWUNTIL number(1) default '0';
 
+ALTER TABLE melete_module_shdates add ALLOWUNTIL_DATE date default NULL;
 
-ALTER TABLE melete_module_shdates add column ALLOWUNTIL_DATE datetime default NULL;
+alter table melete_module add MODIFY_USER_ID varchar(99) default '' NOT NULL;
 
 alter table melete_module drop column LEARN_OBJ;
 alter table melete_module drop column INSTITUTE;
@@ -28,14 +28,12 @@ alter table melete_module drop column CREATED_BY_LNAME;
 alter table melete_module drop column MODIFIED_BY_FNAME;
 alter table melete_module drop column MODIFIED_BY_LNAME;
 
-alter table melete_module add column MODIFY_USER_ID varchar(99) NOT NULL default '';
+alter table melete_section add USER_ID varchar(99) default '' NOT NULL;
+alter table melete_section add MODIFY_USER_ID varchar(99) default '' NOT NULL;
 
-alter table melete_section add column USER_ID varchar(99) NOT NULL default '';
-alter table melete_section add column MODIFY_USER_ID varchar(99) NOT NULL default '';
 alter table melete_section drop column CREATED_BY_FNAME;
 alter table melete_section drop column CREATED_BY_LNAME;
 alter table melete_section drop column MODIFIED_BY_FNAME;
 alter table melete_section drop column MODIFIED_BY_LNAME;
 
-alter table melete_section_track_view add column FIRST_VIEW_DATE datetime default NULL;
-
+alter table melete_section_track_view add FIRST_VIEW_DATE date default NULL;
